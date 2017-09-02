1edc067387775cc61a3b3629289f79cc73830b8c
qydchen <qydchen@gmail.com>
2017-09-02 18:59:24 -0400

    initialize Gathering project

---
+++ frontend/store/store.js
@@ -0,0 +1,16 @@
+import { createStore, applyMiddleware } from 'redux';
+
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+import RootReducer from '../reducers/root_reducer.js';
+
+const configureStore = (preloadedState = {}) => (
+  createStore(
+    RootReducer,
+    preloadedState,
+    applyMiddleware(thunk, logger)
+  )
+);
+
+export default configureStore;
